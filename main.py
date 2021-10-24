2a688ee0b2a36634a5988e683e130e82c42d5e9c
dpizarror <daniel.pizarro@mail.escuelaing.edu.co>
2021-10-24 13:46:54 -0500

    velocidadzrh

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ Editor de Spyder
 Este es un archivo temporal.
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 filename = "JuanDR_Walk01.csv"
 
 # TODO: add your code here
 data = np.genfromtxt(filename,delimiter='\t',skip_header=5)
-rh=data[:,14:17]
+rh=data[:,11:14]
+rt=data[:,17:20]
 sacr=data[:,5:8]
-
-velocidadsacr=np.diff(sacr)
+#zsacr=sacr[:,2]
+velocidadrh=np.absolute(np.diff(rh[:,2]))
+#print(zsacr)
+plt.plot(velocidadrh)
+plt.show()
